e6af2cfc68f215f309dec05a012ab4b8aab03b9a
joseph6720 <joseph.marshal.ng99@gmail.com>
2020-12-09 11:23:42 -0500

    Completed the network scanner project

--- Hacking_tools/Macchanger/macchanger.py
+++ Hacking_tools/Macchanger/macchanger.py
@@ -1,43 +1,40 @@
 #!/usr/bin/python3
 
 import subprocess
-import optparse
+import argparse
 from termcolor import colored
 import re
 
 
 def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface",
-                      help="The interface you want to change")
-    parser.add_option("-m", "--mac", dest="new_mac",
+    parser = argparse.ArgumentParser(
+        description="This is a script to change the mac address of your device")
+    parser.add_argument("-i", "--interface", dest="interface",
+                        help="The interface you want to change", metavar="", required=True)
+    parser.add_argument("-m", "--mac", dest="new_mac", metavar="", required=True,
                       help="The new mac address")
-    (options, arguments) = parser.parse_args()
+    args = parser.parse_args()
 
-    if not options.interface:
-        parser.error(
-            colored("[-] Please specify an interface use --help for more info", "red"))
-    if not options.new_mac:
-        parser.error(
-            colored("[-] Please specify a new mac address use --help for more info", "red"))
-
-    return options
+    return (args.interface, args.new_mac)
 
 
 def get_previous_mac(interface):
-    result = subprocess.check_output(["ifconfig", interface])
-    result_string = result.decode("utf-8")
+    if interface == "eth0":
+        result = subprocess.check_output(["ifconfig", interface])
+        result_string = result.decode("utf-8")
 
-    regex = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', result_string)
+        regex = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', result_string)
 
-    if regex:
-        return regex.group(0)
+        if regex:
+            return regex.group(0)
+        else:
+            print(colored("[-] There was an error", "red"))
     else:
-        print(colored("[-] There was an error", "red"))
+        print(colored("The interface does not exist", "red"))
 
 
 def change_mac(interface, mac, previous_mac):
-    if (interface == "eth0") or (interface == "wlan0"):
+    if (interface == "eth0"):
         print(colored("[+] Closing the network interface", "green"))
         subprocess.call(["ifconfig", interface, "down"])
         check_mac = mac.split(":")
@@ -73,8 +70,8 @@ def change_mac(interface, mac, previous_mac):
             colored("[-] No such interface\n[-] Interfaces are eth0 and wlan0", "red"))
 
 
-options = get_arguments()
-interface = options.interface
-mac = options.new_mac
+args = get_arguments()
+interface = args[0]
+mac = args[1]
 previous_mac = get_previous_mac(interface)
 change_mac(interface, mac, previous_mac)
